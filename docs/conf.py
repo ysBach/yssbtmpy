ee23e8f773a5df517a87a4be20b99191b8f410b6
ysBach <ysbach93@gmail.com>
2024-04-19 22:49:07 +0900

    some setup files

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+project = "yssbtmpy"
+copyright = "2024, ysBach"
+author = "ysBach"
+# version = release = importlib.metadata.version("package")
+
+extensions = [
+    "myst_parser",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.intersphinx",
+    "sphinx.ext.mathjax",
+    "sphinx.ext.napoleon",
+    "sphinx_autodoc_typehints",
+    "sphinx_copybutton",
+]
+
+source_suffix = [".rst", ".md"]
+
+exclude_patterns = [
+    "_build",
+    "**.ipynb_checkpoints",
+    "Thumbs.db",
+    ".DS_Store",
+    ".env",
+    ".venv",
+]
+
+html_theme = "sphinx_rtd_theme"
+
+myst_enable_extensions = [
+    "colon_fence",
+]
+
+intersphinx_mapping = {
+    "python": ("https://docs.python.org/3", None),
+}
+
+nitpick_ignore = [
+    ("py:class", "_io.StringIO"),
+    ("py:class", "_io.BytesIO"),
+]
+
+always_document_param_types = True
